20484c487947a754f85ce2b23d3dc619f40a74a5
scwatts <s.watts2@student.unimelb.edu.au>
2018-04-13 16:19:56 +1000

    Add argument parsing basis

--- pairwise_snp_counter.py
+++ pairwise_snp_counter.py
@@ -1,7 +1,46 @@
 #!/usr/bin/env python3
+import argparse
+import pathlib
+
+
+def get_arguments():
+    parser = argparse.ArgumentParser()
+    subparser = parser.add_subparsers(metavar='mask align', dest='command')
+
+    parser_mask = subparser.add_parser('mask')
+    parser_mask.add_argument('--input_fps', required=True, nargs='+', type=pathlib.Path,
+            help='Input file paths')
+
+    parser_align = subparser.add_parser('align')
+    parser_align.add_argument('--input_fps', required=True, nargs='+', type=pathlib.Path,
+            help='Input file paths')
+
+    # TODO: Perform additional argument parsing, checking
+    args = parser.parse_args()
+    if not args.command:
+        parser.print_help()
+        print('\n', end='')
+        parser.error('command options include mask or align')
+
+    return args
 
 
 def main():
+    # Get commandline arguments
+    args = get_arguments()
+
+    # Execute requested stage
+    if args.command == 'mask':
+        run_mask(args)
+    elif args.command == 'align':
+        run_align(args)
+
+
+def run_mask(args):
+    pass
+
+
+def run_align(args):
     pass
 
 
