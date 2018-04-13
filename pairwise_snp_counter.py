3c28b746e8100dc2b5765176dffee201eb05171d
scwatts <s.watts2@student.unimelb.edu.au>
2018-04-13 16:33:57 +1000

    Add command execution and logging

--- pairwise_snp_counter.py
+++ pairwise_snp_counter.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 import argparse
 import pathlib
+import logging
+import subprocess
 
 
 def get_arguments():
@@ -26,8 +28,9 @@ def get_arguments():
 
 
 def main():
-    # Get commandline arguments
+    # Get commandline arguments and initialise
     args = get_arguments()
+    initialise_logging()
 
     # Execute requested stage
     if args.command == 'mask':
@@ -42,6 +45,33 @@ def run_mask(args):
 
 def run_align(args):
     pass
+
+
+def initialise_logging():
+    # TODO: command line arguments for logging; save to file? print to stdout?
+    # Set up loggers
+    log_filehandler = logging.FileHandler('run.log', mode='w')
+    log_streamhandler = logging.StreamHandler()
+    log_format = logging.Formatter(fmt='%(asctime)s %(message)s', datefmt='%d/%m/%Y %H:%M:%S')
+    log_filehandler.setFormatter(log_format)
+    log_streamhandler.setFormatter(log_format)
+
+    # Add log handles to root logger and set log level
+    logger = logging.getLogger()
+    logger.addHandler(log_filehandler)
+    logger.addHandler(log_streamhandler)
+    # TODO: expose this option to the command line
+    logger.setLevel(logging.INFO)
+
+
+def execute_command(command):
+    result = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True,
+                            encoding='utf-8')
+    if result.returncode != 0:
+        # TODO: are we happy with logging over multiple lines?
+        logging.critical('Failed to run command: %s', result.args)
+        logging.critical('stdout: %s', result.stdout)
+        logging.critical('stderr: %s', result.stderr)
 
 
 if __name__ == '__main__':
