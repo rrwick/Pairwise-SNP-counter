751d6a8b843b5c17b6b37d66826aedf471110ad4
scwatts <s.watts2@student.unimelb.edu.au>
2018-04-13 15:37:54 +1000

    Add setup.py and pairwise_snp_counter.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+import setuptools
+
+
+# Set package details
+package_name = 'pairwise_snp_counter'
+package_description = 'Call confident SNPs in pairwise comparison'
+package_version = '0.0.1'
+author = 'Holt Lab Coding Group'
+licence = 'GPLv3'
+
+
+# Call setup
+setuptools.setup(
+        name=package_name,
+        version=package_version,
+        licence=licence,
+        test_suite='tests',
+        packages=setuptools.find_packages(),
+        scripts=['pairwise_snp_counter.py']
+        )
