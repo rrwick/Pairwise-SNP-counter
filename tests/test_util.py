de0566961a02f6c1b03b74813b8f9eafb31d4ff1
Ryan Wick <rrwick@gmail.com>
2018-04-16 10:46:26 +1000

    Print test result to debug Travis-CI failure

--- tests/test_util.py
+++ tests/test_util.py
@@ -8,6 +8,9 @@ class TestUtilFunctions(unittest.TestCase):
 
     def test_execute_command(self):
         result = psc.execute_command('echo test', check=False)
+        print(result.returncode)
+        print(result.stdout)
+        print(result.stderr)
         self.assertEqual(result.returncode, 0)
         self.assertEqual(result.stdout, 'test\n')
         self.assertEqual(result.stderr, '')
