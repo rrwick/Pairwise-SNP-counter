7b98ee646406515e02480df59f6fffd522e29fca
Ryan Wick <rrwick@gmail.com>
2018-04-16 16:38:40 +1000

    Add GPL notices

--- pairwise_snp_counter.py
+++ pairwise_snp_counter.py
@@ -1,4 +1,22 @@
 #!/usr/bin/env python3
+"""
+Copyright 2018 Ryan Wick (rrwick@gmail.com), Stephen Watts, Alex Tokolyi
+https://github.com/rrwick/Pairwise-SNP-counter
+
+This is a command line tool for counting the single-nucleotide differences (a.k.a. SNPs) between
+two closely related assemblies.
+
+TODO: add some example commands here.
+
+This program is free software: you can redistribute it and/or modify it under the terms of the GNU
+General Public License as published by the Free Software Foundation, either version 3 of the
+License, or (at your option) any later version. This program is distributed in the hope that it
+will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details. You should
+have received a copy of the GNU General Public License along with this program.  If not, see
+<http://www.gnu.org/licenses/>.
+"""
+
 import argparse
 from distutils.version import LooseVersion
 import logging
