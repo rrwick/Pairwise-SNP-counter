112647dfde12ffada98840f5e6daf7f392175402
Ryan Wick <rrwick@gmail.com>
2018-04-16 10:14:12 +1000

    Add some tests for mpileup parsing

--- tests/test_scores.py
+++ tests/test_scores.py
@@ -2,6 +2,7 @@ import unittest
 
 
 import pairwise_snp_counter as psc
+import pathlib
 
 
 class TestScoreThreshold(unittest.TestCase):
@@ -41,3 +42,51 @@ class TestScoreThreshold(unittest.TestCase):
         self.assertEqual(psc.get_score_threshold(scores, 75.0), 15)
         self.assertEqual(psc.get_score_threshold(scores, 100.0), 20)
 
+
+class TestMpileupOutputParsing(unittest.TestCase):
+
+    def test_get_base_scores_from_mpileup_output_1(self):
+        """
+        This mpileup output (mpileup_output_1) comes from an Illumina read assembly.
+        """
+        mpileup_output_filename = pathlib.Path(__file__).parent / 'mpileup_output_1'
+        with open(mpileup_output_filename, 'rt') as mpileup_output_file:
+            mpileup_output = mpileup_output_file.read()
+        scores = psc.get_base_scores_from_mpileup_output(mpileup_output)
+
+        # Test some individual bases.
+        self.assertAlmostEqual(scores['chromosome'][0], 6 / 14)
+        self.assertAlmostEqual(scores['chromosome'][1], 5 / 20)
+        self.assertAlmostEqual(scores['chromosome'][50], 86 / 88)
+        self.assertAlmostEqual(scores['chromosome'][51], 88 / 89)
+        self.assertAlmostEqual(scores['chromosome'][52], 89 / 89)
+
+        # These bases contain an indel which covers positions 57 and 58 (58 and 59 in the VCF's
+        # 1-based indexing). This means that position 58 should not get a score of 1 as indicated
+        # by its VCF line, but rather a score of 89/90 as indicated by the indel VCF line.
+        self.assertAlmostEqual(scores['chromosome'][56], 90 / 90)
+        self.assertAlmostEqual(scores['chromosome'][57], 89 / 90)
+        self.assertAlmostEqual(scores['chromosome'][58], 89 / 90)
+        self.assertAlmostEqual(scores['chromosome'][59], 90 / 91)
+
+    def test_get_base_scores_from_mpileup_output_2(self):
+        """
+        This mpileup output (mpileup_output_2) comes from a Nanopore read assembly, where indels
+        are much more common.
+        """
+        mpileup_output_filename = pathlib.Path(__file__).parent / 'mpileup_output_2'
+        with open(mpileup_output_filename, 'rt') as mpileup_output_file:
+            mpileup_output = mpileup_output_file.read()
+        scores = psc.get_base_scores_from_mpileup_output(mpileup_output)
+
+        # This base is (unusually for a Nanopore assembly) not covered by an indel.
+        self.assertAlmostEqual(scores['chromosome'][29], 50 / 57)
+
+        # These bases are covered by an indel as well as the normal substitution line, so should
+        # have the worst score of the two.
+        self.assertAlmostEqual(scores['chromosome'][30], 46 / 57)
+        self.assertAlmostEqual(scores['chromosome'][50], 34 / 59)
+
+        # This base is covered by two indels as well as the normal substitution line, so should
+        # have the worst score of the three.
+        self.assertAlmostEqual(scores['chromosome'][226], 30 / 60)
