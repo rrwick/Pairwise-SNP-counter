202cc8043e521caf5a5bb35991baee1247e4a7ed
Ryan Wick <rrwick@gmail.com>
2018-04-16 10:42:16 +1000

    Fix util test to work with different kinds of echo

--- tests/test_util.py
+++ tests/test_util.py
@@ -7,7 +7,7 @@ import pairwise_snp_counter as psc
 class TestUtilFunctions(unittest.TestCase):
 
     def test_execute_command(self):
-        result = psc.execute_command('echo -n test', check=False)
+        result = psc.execute_command('echo test', check=False)
         self.assertEqual(result.returncode, 0)
-        self.assertEqual(result.stdout, 'test')
+        self.assertEqual(result.stdout, 'test\n')
         self.assertEqual(result.stderr, '')
